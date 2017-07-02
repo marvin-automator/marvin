a0ba7e98f722c399d68e5eda73c86f07b68ad031
Frederik Creemers <frederikcreemers@gmail.com>
2017-07-02 17:02:18 +0200

    database helpers

--- storage/db.go
+++ storage/db.go
@@ -3,18 +3,45 @@ package storage
 import (
 	"github.com/boltdb/bolt"
 	"log"
+	"path"
+	"os"
+	"io/ioutil"
 )
 
 var db *bolt.DB
 
-func init() {
+func Setup() {
+	wd, err := os.Getwd()
+	if err != nil {
+		wd = "."
+	}
+	SetupInFile(path.Join(wd, "marvin.db"))
+}
+
+func SetupTestDB() {
+	tempDir := os.TempDir()
+	SetupInFile(path.Join(tempDir, "marvin.db"))
+}
+
+func SetupInFile(fname string) {
 	var err error
 
-	db, err = bolt.Open("marvin.db", 0600, nil)
+	db, err = bolt.Open(fname, 0600, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
+
+func CloseDB() {
+	db.Close()
+}
+
+
+func DeleteDBFile(){
+	db.Close()
+	os.Remove(db.Path())
+}
+
 
 // A Store is the way you access stored data. When you know you're not going to need to write data, open a read-only
 // store. There can only be one writable store open at a time, but multiple read-only stores.
