726ae3ac48e1c5f41e681ec880253da960a61102
Frederik Creemers <frederikcreemers@gmail.com>
2017-07-02 19:35:58 +0200

    Add GetAccountByEmail method.

--- storage/accounts.go
+++ storage/accounts.go
@@ -3,7 +3,11 @@ package storage
 import (
 	"github.com/bigblind/marvin/domain"
 	"github.com/boltdb/bolt"
+	"errors"
 )
+
+var accountFound = errors.New("Account found")
+var AccountNotFound = errors.New("Account not found")
 
 func (s Store) SaveAccount(acct domain.Account) error {
 	bucket, err := s.getOrCreateAccountsBucket()
@@ -28,6 +32,33 @@ func (s Store) GetAccountByID(aid string) (domain.Account, error) {
 	act := domain.Account{}
 	ab := bucket.Get([]byte(aid))
 	return act, bytesToData(&act, ab)
+}
+
+func (s Store) GetAccountByEmail(email string) (domain.Account, error) {
+	bucket, err := s.getOrCreateAccountsBucket()
+	if err != nil {
+		return domain.Account{}, err
+	}
+
+	act := domain.Account{}
+	err = bucket.ForEach(func(_, ab []byte) error {
+		err = bytesToData(&act, ab)
+		if err == nil && act.Email == email {
+			// Returning an error stops the iteration.
+			// ForEach then returns the same error.
+			// So we check for this error value later.
+			return accountFound
+		}
+		return nil
+	})
+
+	if err == nil {
+		return domain.Account{}, AccountNotFound
+	} else if err != accountFound {
+		return domain.Account{}, err
+	}
+
+	return act, nil
 }
 
 func (s Store) getOrCreateAccountsBucket() (*bolt.Bucket, error) {
--- storage/accounts_test.go
+++ storage/accounts_test.go
@@ -25,3 +25,43 @@ func TestSaveAndGetAccount(t *testing.T) {
 
 	})
 }
+
+func TestGetAccountByEmailExists(t *testing.T) {
+	WithTestDB(t, func(s Store) {
+		a1 := domain.Account{"042", "foo@example.com", []byte("nothashed")}
+		err := s.SaveAccount(a1)
+		if err != nil {
+			t.Error(err)
+		}
+
+		a2, err := s.GetAccountByEmail("foo@example.com")
+		if err != nil {
+			t.Error(err)
+		}
+
+		if a1.ID != a2.ID || a1.Email != a2.Email || !bytes.Equal(a1.PasswordHash, a2.PasswordHash){
+			t.Errorf("The saved account does not equal the retrieved account: \n%v\n%v", a1, a2)
+		}
+
+	})
+}
+
+func TestGetAccountByEmailDoesNotExist(t *testing.T) {
+	WithTestDB(t, func(s Store) {
+		a1 := domain.Account{"042", "foo@example.com", []byte("nothashed")}
+		err := s.SaveAccount(a1)
+		if err != nil {
+			t.Error(err)
+		}
+
+		a2, err := s.GetAccountByEmail("bar@example.com")
+		if err != AccountNotFound {
+			t.Errorf("Account should not be found, but didn't get AccountNotFound error. got %v instead.", err)
+		}
+		if a2.ID != "" || a2.Email != "" || !bytes.Equal(a2.PasswordHash, []byte{}) {
+			t.Error("The passed in account should not be altered.")
+		}
+	})
+}
+
+
