720aeea220918f4aae5b414f2cb046fd8aedb5b1
Frederik Creemers <frederikcreemers@gmail.com>
2017-07-06 00:34:36 +0200

    Split off login-centric interactors into their own file and tests

--- accounts/interactors/accounts.go
+++ accounts/interactors/accounts.go
@@ -1,62 +1,13 @@
 package interactors
 
 import (
-	"errors"
 	"github.com/bigblind/marvin/accounts/domain"
-	configdomain "github.com/bigblind/marvin/config/domain"
 )
-
-// Returned when trying to log in when accounts are disabled
-var ErrAccountsDisabled = errors.New("Can not log in when accounts are disabled.")
-
-// Returned when the password doesn't match the account's password, or when the account isn't found
-var ErrLoginFailed = errors.New("Incorrect email/password combination.")
 
 // The Account type that the actions return to handlers.
 type Account struct {
 	ID    string
 	Email string
-}
-
-type Login struct {
-	AccountStore domain.AccountStore
-	ConfigStore  configdomain.ConfigStore
-}
-
-func (l Login) Execute(email, password string) (Account, error) {
-	config, err := l.ConfigStore.GetConfig()
-	if err != nil {
-		return Account{}, err
-	}
-
-	if !config.AccountsEnabled {
-		return Account{}, ErrAccountsDisabled
-	}
-
-	act, err := l.AccountStore.GetAccountByEmail(email)
-
-	if err == domain.ErrAccountNotFound {
-		return Account{}, ErrLoginFailed
-	}
-
-	if err != nil {
-		return Account{}, err
-	}
-
-	result, err := act.CheckPassword(password)
-	if err != nil {
-		return Account{}, err
-	}
-
-	if !result {
-		return Account{}, ErrLoginFailed
-	}
-
-	return Account{act.ID, act.Email}, nil
-}
-
-type ILogin interface {
-	Execute(email, password string) (bool, error)
 }
 
 type CreateAccount struct {
---
+++ accounts/interactors/login_test.go
@@ -0,0 +1,82 @@
+package interactors
+
+import (
+	"errors"
+	"github.com/bigblind/marvin/accounts"
+	"github.com/bigblind/marvin/accounts/domain"
+	"github.com/bigblind/marvin/config"
+	configdomain "github.com/bigblind/marvin/config/domain"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestSuccessfulLogin(t *testing.T) {
+	acc, err := domain.NewAccount("test@example.com", "pwd")
+	require.NoError(t, err)
+	exp := Account{acc.ID, "test@example.com"}
+	ma := accounts.NewMockAccountStore()
+	ma.On("GetAccountByEmail", "test@example.com").Return(acc, nil)
+	mc := config.NewMockConfigStore()
+	mc.On("GetConfig").Return(configdomain.DefaultConfig, nil)
+
+	login := Login{ma, mc}
+	res, err := login.Execute("test@example.com", "pwd")
+	require.Equal(t, exp, res)
+}
+
+func TestWrongPassword(t *testing.T) {
+	acc, err := domain.NewAccount("test@example.com", "pwd")
+	ma := accounts.NewMockAccountStore()
+	ma.On("GetAccountByEmail", "test@example.com").Return(acc, nil)
+	mc := config.NewMockConfigStore()
+	mc.On("GetConfig").Return(configdomain.DefaultConfig, nil)
+	require.NoError(t, err)
+
+	login := Login{ma, mc}
+	_, err = login.Execute("test@example.com", "incorrect")
+	require.EqualError(t, err, ErrLoginFailed.Error())
+}
+
+func TestAccountNotFoundReturnsFailedLogin(t *testing.T) {
+	ma := accounts.NewMockAccountStore()
+	ma.On("GetAccountByEmail", "test@example.com").Return(domain.Account{}, domain.ErrAccountNotFound)
+	mc := config.NewMockConfigStore()
+	mc.On("GetConfig").Return(configdomain.DefaultConfig, nil)
+
+	login := Login{ma, mc}
+	_, err := login.Execute("test@example.com", "pwd")
+	require.EqualError(t, err, ErrLoginFailed.Error(), "We need to return the same error as if the password was wrong.")
+}
+
+func TestConfigStoreError(t *testing.T) {
+	ma := accounts.NewMockAccountStore()
+	configError := errors.New("something went wrong")
+	mc := config.NewMockConfigStore()
+	mc.On("GetConfig").Return(configdomain.DefaultConfig, configError)
+
+	login := Login{ma, mc}
+	_, err := login.Execute("test@example.com", "pwd")
+	require.EqualError(t, err, configError.Error())
+}
+
+func TestAccountStoreError(t *testing.T) {
+	accountError := errors.New("something went wrong")
+	ma := accounts.NewMockAccountStore()
+	ma.On("GetAccountByEmail", "test@example.com").Return(domain.Account{}, accountError)
+	mc := config.NewMockConfigStore()
+	mc.On("GetConfig").Return(configdomain.DefaultConfig, nil)
+
+	login := Login{ma, mc}
+	_, err := login.Execute("test@example.com", "pwd")
+	require.EqualError(t, err, accountError.Error())
+}
+
+func TestAccountsDisabled(t *testing.T) {
+	ma := accounts.NewMockAccountStore()
+	mc := config.NewMockConfigStore()
+	mc.On("GetConfig").Return(configdomain.Config{AccountsEnabled: false}, nil)
+
+	login := Login{ma, mc}
+	_, err := login.Execute("test@example.com", "pwd")
+	require.EqualError(t, err, ErrAccountsDisabled.Error())
+}
