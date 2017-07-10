8ceb14661366fc8824e4d59e22e5275ebb519b6d
Frederik Creemers <frederikcreemers@gmail.com>
2017-07-10 19:54:48 +0200

    Update main to deal with the change of app location.

--- main.go
+++ main.go
@@ -4,12 +4,12 @@ import (
 	"github.com/gobuffalo/envy"
 	"log"
 	"github.com/bigblind/marvin/storage"
+	"github.com/bigblind/marvin/app"
 )
 
 func main() {
 	port := envy.Get("PORT", "3000")
 	storage.Setup()
-	App()
-	log.Printf("App: %#v", app)
-	log.Fatal(app.Start(port))
+	marvin := app.App()
+	log.Fatal(marvin.Start(port))
 }
