7a170d5017bc6500c18b6a48a1124d6e1129f40f
Frederik Creemers <frederikcreemers@gmail.com>
2017-07-16 19:50:36 +0200

    Add Logger interface in the interactor layer

---
+++ app/interactors/logger.go
@@ -0,0 +1,53 @@
+package interactors
+
+import "github.com/gobuffalo/buffalo"
+
+// Logger interface is used throughout Marvin
+// to log all the things.
+type Logger interface {
+	// WithField will return a logger that will log the given field with value for each log message
+	WithField(string, interface{}) Logger
+	// WithFields is equivalent to calling WithField for each key-value pair in the map.
+	WithFields(map[string]interface{}) Logger
+	// Log a debug message with a format string
+	Debugf(string, ...interface{})
+	//Log an info message with a format string
+	Infof(string, ...interface{})
+	//Log a warning message with a format string
+	Warnf(string, ...interface{})
+	//Log an error message with a format string
+	Errorf(string, ...interface{})
+	//Log a fatal message with a format string
+	Fatalf(string, ...interface{})
+	// Log a debug message
+	Debug(...interface{})
+	// Log an info mesage
+	Info(...interface{})
+	// Log a warning message
+	Warn(...interface{})
+	// Log an error message
+	Error(...interface{})
+	// Log a fatal message
+	Fatal(...interface{})
+	// 😱 Panic with this message
+	Panic(...interface{})
+}
+
+type loggerWrapper struct {
+	buffalo.Logger
+}
+
+func LoggerFromBuffaloLogger(l buffalo.Logger) Logger {
+	lw := loggerWrapper{l}
+	return &lw
+}
+
+// WithFields eturns a logger that logs the given fields with each message
+func (l *loggerWrapper) WithFields(fields map[string]interface{}) Logger {
+	return LoggerFromBuffaloLogger(l.Logger.WithFields(fields))
+}
+
+// WithField returns a logger that logs the field with every message
+func (l *loggerWrapper) WithField(name string, value interface{}) Logger {
+	return l.WithFields(map[string]interface{}{name: value})
+}
--- main.go
+++ main.go
@@ -5,11 +5,14 @@ import (
 	"github.com/bigblind/marvin/storage"
 	"github.com/gobuffalo/envy"
 	"log"
+	"github.com/bigblind/marvin/actions/interactors/execution"
+	"github.com/bigblind/marvin/app/interactors"
 )
 
 func main() {
 	port := envy.Get("PORT", "3000")
 	storage.Setup()
 	marvin := app.App()
+	execution.SetupExecutionEnvironment(marvin.Context, interactors.LoggerFromBuffaloLogger(marvin.Logger))
 	log.Fatal(marvin.Start(port))
 }
