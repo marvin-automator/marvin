88f9f29d43b16ed1aa27c94867d24a9e73e42f43
Frederik Creemers <frederikcreemers@gmail.com>
2017-07-18 20:24:11 +0200

    Add identityprovider domain
    This package will be used by the action executor to provide access to
    3rd party identities to actions, so not every action provider has to write their own oauth code.

---
+++ identityproviders/domain/oauth.go
@@ -0,0 +1,30 @@
+package domain
+
+import "net/http"
+
+type Identity struct {
+	Name string
+	ImageURL string
+	// Subtext can be used for other information that might be helpful to users when selecting an account.
+	Subtext string
+	// ProviderID is the ID of this identity, assigned by the identity provider
+	ProviderID string
+	// The access token obtained from the provider
+	Token string
+}
+
+// IdentityFetcher should be implemented by ActionProviders that want to use
+// identity protocols from this package. They're responsible for gathering the account details
+// that are not identical across implementations of the protocols
+type IdentityFetcher interface{
+	// Given an http.Client that will automatically authorize requests,
+	// FetchIdentity should fetch the identity associated with the credentials.
+	FetchIdentity(c http.Client) Identity
+}
+
+// An IdentityStore persists 3rd party identities
+type IdentityStore interface {
+	SaveIdentity(account, provider string, id Identity) error
+	GetAccountIdentitiesForProvider(account, provider string) []Identity
+	DeleteIdentity(account, provider string, i Identity) error
+}
