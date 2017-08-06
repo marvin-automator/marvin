4885b887dd4a4480296aba7285117e547468a442
Frederik Creemers <frederikcreemers@gmail.com>
2017-08-06 18:01:58 +0200

    Fix typo in template name

--- app/handlers/app.go
+++ app/handlers/app.go
@@ -5,5 +5,5 @@ import "github.com/marvin-automator/marvin/handlers"
 // AppPage renders the page that contains the main JavaScript app.
 func AppPage(c handlers.Context) error {
 	c.Logger().Debug("Rendering JS App view")
-	return c.Render(200, c.Renderer().HTML("jsapp.html)"))
+	return c.Render(200, c.Renderer().HTML("jsapp.html"))
 }
