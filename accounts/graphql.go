549b8d8bce0e7cda36383f683eb7b10e26e68afc
Frederik Creemers <frederikcreemers@gmail.com>
2017-08-08 11:37:13 +0200

    Add currentAccount GraphQL field

--- accounts/graphql.go
+++ accounts/graphql.go
@@ -1,9 +1,12 @@
 package accounts
 
-import "github.com/graphql-go/graphql"
+import (
+	"github.com/graphql-go/graphql"
+	"github.com/marvin-automator/marvin/handlers"
+)
 
 // GraphQLAccount is the GraphQL representation of an Account.
-var GraphQLAccount = graphql.NewObject(graphql.ObjectConfig{
+var GQLAccount = graphql.NewObject(graphql.ObjectConfig{
 	Name: "Account",
 	Fields: graphql.Fields{
 		"id": &graphql.Field{Type: graphql.ID},
@@ -11,3 +14,10 @@ var GraphQLAccount = graphql.NewObject(graphql.ObjectConfig{
 	},
 })
 
+var GQLCurrentAccount = graphql.Field{
+	Name: "currentAccount",
+	Type: GraphQLAccount,
+	Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+		return CurrentAccount(p.Context.(handlers.Context)), nil
+	},
+}
--- graphql/schema.go
+++ graphql/schema.go
@@ -1,15 +1,13 @@
 package graphql
 
-import "github.com/graphql-go/graphql"
+import (
+	"github.com/graphql-go/graphql"
+	"github.com/marvin-automator/marvin/accounts"
+)
 
 func schema() *graphql.Schema {
 	fields := graphql.Fields{
-		"test": &graphql.Field{
-			Type: graphql.String,
-			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				return "1, 2, 3", nil
-			},
-		},
+		"currentAccount": &accounts.GQLCurrentAccount,
 	}
 
 	rq := graphql.ObjectConfig{
