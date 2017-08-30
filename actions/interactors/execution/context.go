99471f97bc41a2377b988687825f2190a540cd3e
Frederik Creemers <frederikcreemers@gmail.com>
2017-08-30 10:31:51 +0200

    Add configurationContext implementation
    The ActionContext implementation now also ddelegates some of its tasks to configurationContext.

--- actions/interactors/execution/context.go
+++ actions/interactors/execution/context.go
@@ -35,65 +35,103 @@ func getChoreContext(cid string) choreContext {
 		logger:  globalActionLogger.WithField("chore", cid),
 	}
 
+	choreContexts[cid] = cc
+
 	go func() {
 		<-ctx.Done()
 		cc.logger.Info("Stopping chore %v", cid)
+		delete(choreContexts, cid)
 	}()
 
 	return cc
 }
 
-type actionContext struct {
-	exec       *Executor
-	context    context.Context
-	Cancel     context.CancelFunc
-	isTestCall bool
-	logger     appdomain.Logger
+type configurationContext struct {
+	context 	context.Context
+	logger		appdomain.Logger
+	exec		*Executor
 
-	chore      domain.Chore
-	instance   domain.ActionInstance
-	actionMeta domain.ActionMeta
+	account		string
+	instance	domain.ActionInstance
+	actionMeta	domain.ActionMeta
+}
+
+func newConfigurationContext(ex *Executor, account string, log appdomain.Logger, ac domain.BaseAction, inst domain.ActionInstance, ctx context.Context) *configurationContext {
+	c := configurationContext{
+		context: ctx,
+		logger: log,
+		exec: ex,
+		account: account,
+		instance: inst,
+		actionMeta: ac.Meta(),
+	}
+	return &c
+}
+
+func (c *configurationContext) InstanceStore() domain.KVStore {
+	panic("implement me")
+}
+
+func (c *configurationContext) GetCallbackURL(path string) string {
+	panic("implement me")
+}
+
+func (c *configurationContext) GlobalConfig() interface{} {
+	panic("implement me")
+}
+
+func (c *configurationContext) Logger() appdomain.Logger {
+	return c.logger
+}
+
+// HTTPClient returns a http.Client that, if the action requires an identity, and everything is configured correctly,
+// will automatically make requests with the correct credentials.
+// In any other case, this returns http.DefaultClient.
+func (c *configurationContext) HTTPClient() *http.Client {
+	ip := idinteractors.IdentityProvider{
+		Provider:      c.exec.registry.Provider(c.instance.ActionProvider),
+		IdentityStore: c.exec.identityStore,
+		Logger:        c.logger,
+	}
+
+	gc := c.GlobalConfig()
+	gcv := reflect.ValueOf(gc)
+	cid := gcv.FieldByName("ClientID").String()
+	csec := gcv.FieldByName("ClientSecret").String()
+
+	return ip.GetHTTPClient(cid, csec, c.account, c.instance.Identity)
+}
+
+type actionContext struct {
+	*configurationContext
+
+	Cancel		context.CancelFunc
+	isTestCall	bool
+	chore		domain.Chore
 }
 
 // newActionContext creates a new ActionContext. ch should be the chore this action is executing in.
 func newActionContext(ex *Executor, ch domain.Chore, ac domain.BaseAction, inst domain.ActionInstance) *actionContext {
 	cc := getChoreContext(ch.ID)
 	ctx, cancel := context.WithCancel(cc)
+	log := cc.logger.WithFields(map[string]interface{}{
+		"action":          ac.Meta().Key,
+		"action_instance": inst.ID,
+	})
+
+	configCtx := newConfigurationContext(ex, ch.Owner, log, ac, inst, ctx)
 	a := actionContext{
-		exec:       ex,
-		context:    ctx,
+		configurationContext: configCtx,
+
 		Cancel:     cancel,
 		isTestCall: false,
-		logger: cc.logger.WithFields(map[string]interface{}{
-			"action":          ac.Meta().Key,
-			"action_instance": inst.ID,
-		}),
-
 		chore:      ch,
-		instance:   inst,
-		actionMeta: ac.Meta(),
 	}
 	return &a
 }
 
 // InvocationStore returns a store valid for the duration of this invocation
 func (a *actionContext) InvocationStore() domain.KVStore {
-	panic("implement me")
-}
-
-// InstanceStore returns a store with data that is specific to this instance.
-func (a *actionContext) InstanceStore() domain.KVStore {
-	panic("implement me")
-}
-
-// AccountGlobalStore stores data that's accessible to all actions in this provider,
-// Data is partitioned by account.
-func (a *actionContext) AccountGlobalStore() domain.KVStore {
-	panic("implement me")
-}
-
-// GetCallbackURL returns a url that actions can use to receive information from other (web) applications
-func (a *actionContext) GetCallbackURL(path string) string {
 	panic("implement me")
 }
 
@@ -105,11 +143,6 @@ func (a *actionContext) IsTestCall() bool {
 // MarkTestCall marks the call that this context will be given as a test call
 func (a *actionContext) MarkTestCall() {
 	a.isTestCall = true
-}
-
-// GlobalConfig returns the global configuration object. This object can only be changed on the settingss page, so changes to the object will not be persisted.
-func (a *actionContext) GlobalConfig() interface{} {
-	panic("implement me")
 }
 
 // Output should be called by the action when it wants to send output to the next action
@@ -126,22 +159,4 @@ func (a *actionContext) Done() {
 // The action should call Error to log en error, in cases where it isn't possible or appropriate to return an error.
 func (a *actionContext) Logger() appdomain.Logger {
 	return a.logger
-}
-
-// HTTPClient returns a http.Client that, if the action requires an identity, and everything is configured correctly,
-// will automatically make requests with the correct credentials.
-// In any other case, this returns http.DefaultClient.
-func (a *actionContext) HTTPClient() *http.Client {
-	ip := idinteractors.IdentityProvider{
-		Provider:      a.exec.registry.Provider(a.instance.ActionProvider),
-		IdentityStore: a.exec.identityStore,
-		Logger:        a.logger,
-	}
-
-	gc := a.GlobalConfig()
-	gcv := reflect.ValueOf(gc)
-	cid := gcv.FieldByName("ClientID").String()
-	csec := gcv.FieldByName("ClientSecret").String()
-
-	return ip.GetHTTPClient(cid, csec, a.chore.Owner, a.instance.Identity)
 }
