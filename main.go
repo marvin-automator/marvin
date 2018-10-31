d0c3cf669e12dd235af4fb71f51baeaaf509d067
Frederik Creemers <frederikcreemers@gmail.com>
2018-10-31 21:23:38 +0100

    reorder imports in main

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package marvin
 
 import (
 	_ "github.com/marvin-automator/marvin/actions"
+	_ "github.com/marvin-automator/marvin/internal/actions/builtin/http"
 	_ "github.com/marvin-automator/marvin/internal/actions/builtin/time"
 	_ "github.com/marvin-automator/marvin/internal/chores"
-	_ "github.com/marvin-automator/marvin/internal/actions/builtin/http"
 )
