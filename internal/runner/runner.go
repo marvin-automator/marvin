d8b448930f505b85907746d206d792b2060835a7
Frederik Creemers <frederikcreemers@gmail.com>
2018-11-01 02:17:59 +0100

    Runner recovers if a task panics.

--- internal/runner/runner.go
+++ internal/runner/runner.go
@@ -14,11 +14,11 @@ type Task func(doneCh chan<- struct{})
 
 type TaskRunner interface {
 	Start(ctx context.Context)
-	Run(t Task)
+	Run(t Task) error
 }
 
 func NewRunner() TaskRunner {
-	return &taskRunner{doneCh: make(chan struct{}, 10)}
+	return &taskRunner{doneCh: make(chan struct{}, 100)}
 }
 
 func (tr *taskRunner) Start(ctx context.Context) {
@@ -33,7 +33,14 @@ func (tr *taskRunner) Start(ctx context.Context) {
 	tr.wg.Wait()
 }
 
-func (tr *taskRunner) Run(t Task) {
+func (tr *taskRunner) Run(t Task) (err error) {
+	defer func(){
+		v := recover()
+		if v != nil {
+			tr.doneCh<- struct{}{}
+		}
+	}()
 	tr.wg.Add(1)
 	t(tr.doneCh)
+	return nil
 }
--- internal/runner/runner_test.go
+++ internal/runner/runner_test.go
@@ -15,6 +15,7 @@ func TestTaskRunner_Run_No_Tasks(t *testing.T) {
 	tr.Start(ctx)
 }
 
+// Test that Start() returns upon cancelling the context, but waits for running functions to finish.
 func TestTaskRunner_Run(t *testing.T) {
 	r := require.New(t)
 	res := "not run"
@@ -32,4 +33,20 @@ func TestTaskRunner_Run(t *testing.T) {
 	tr.Start(ctx)
 
 	r.Equal("run", res)
+}
+
+// Test that Start() still returns normally when a function passed to Run() panics
+func TestTaskRunner_Run_Panic(t *testing.T) {
+	r := require.New(t)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	tr := NewRunner()
+	tr.Run(func(doneCh chan<- struct{}) {
+		time.AfterFunc(5*time.Millisecond, func() {
+			panic("heyo!")
+		})
+	})
+
+	time.AfterFunc(1*time.Millisecond, cancel)
+	tr.Start(ctx)
 }
