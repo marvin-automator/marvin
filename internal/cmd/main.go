c1fe4dabb7a753d37a2f9950b5e6458e737dc153
Frederik Creemers <frederikcreemers@gmail.com>
2018-11-03 23:09:30 +0100

    Create a default config file if it doesn't exist.

--- internal/cmd/main.go
+++ internal/cmd/main.go
@@ -1,8 +1,9 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
-	homedir "github.com/mitchellh/go-homedir"
+	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"os"
@@ -10,11 +11,9 @@ import (
 
 
 var rootCmd = &cobra.Command{
-	Use:   "hugo",
-	Short: "Hugo is a very fast static site generator",
-	Long: `A Fast and Flexible Static Site Generator built with
-                love by spf13 and friends in Go.
-                Complete documentation is available at http://hugo.spf13.com`,
+	Use:   "marvin",
+	Short: "Marvin is a tool for automating all sorts of things.",
+	Long: `Here I am, brain the size of a planet`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// Do Stuff Here
 	},
@@ -32,6 +31,8 @@ var cfgFile string
 func init() {
 	cobra.OnInitialize(initConfig)
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is ./marvin.yaml, and if that doesn't exist, $HOME/.marvin.yaml)")
+	rootCmd.Flags().StringP("template_path", "t", "chore_templates", "The path where chore templates are stored. Relative paths are resolved relative to the config file that was loaded.")
+	viper.BindPFlag("template_path", rootCmd.Flags().Lookup("template_path"))
 }
 
 func initConfig() {
@@ -43,19 +44,54 @@ func initConfig() {
 		// Find home directory.
 		home, err := homedir.Dir()
 		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
+			fmt.Println("Couldn't determine the home directory:", err, "\nSkipping looking for config files there.")
+		} else {
+			viper.AddConfigPath(home)
 		}
 
 		// Search config in home directory with name ".cobra" (without extension).
 		viper.AddConfigPath(".")
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".marvin")
 		viper.SetConfigName("marvin")
 	}
 
 	if err := viper.ReadInConfig(); err != nil {
-		fmt.Println("Can't read config:", err)
-		os.Exit(1)
+		switch err.(type) {
+		case viper.ConfigFileNotFoundError:
+			if cfgFile != "" {
+				fmt.Println("No config file found at the given location:", cfgFile)
+				os.Exit(1)
+			}
+			err2 := createConfigFile()
+			if err2 != nil {
+				fmt.Println(err2)
+			}
+
+		default:
+			fmt.Println("Can't read config:", err)
+			os.Exit(1)
+		}
+	} else {
+		fmt.Printf("Loaded config file from: %s\n", viper.ConfigFileUsed())
 	}
+}
+
+func createConfigFile() error {
+	fmt.Print("No configuration found. We'll create a default one for you. Where should we create it? (leave blank to use ./marvin.yaml): ")
+
+	r := bufio.NewReader(os.Stdin)
+	l, _, err := r.ReadLine()
+	if err != nil {
+		return err
+	}
+
+	name := string(l)
+	if name == "" {
+		name = "./marvin.yaml"
+	}
+
+	if _, err := os.Stat(name); !os.IsNotExist(err) {
+		return fmt.Errorf("file exists at %v", name)
+	}
+
+	return viper.WriteConfigAs(name)
 }
