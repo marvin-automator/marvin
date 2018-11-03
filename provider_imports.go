c392b1d86d4bfdc2ae6bf6a097c1446c28e998e5
Frederik Creemers <frederikcreemers@gmail.com>
2018-11-03 21:17:26 +0100

    Ensure that internal/actions gets called so the registry is initialized.

--- provider_imports.go
+++ provider_imports.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	_ "github.com/marvin-automator/marvin/actions" //
+	_ "github.com/marvin-automator/marvin/internal/actions" // make sure the registry gets initialized.
 
 	_ "github.com/marvin-automator/marvin/internal/actions/builtin/http"
 	_ "github.com/marvin-automator/marvin/internal/actions/builtin/time"
