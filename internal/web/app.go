c1d8923df289d45bbfebff0d704aa3ca96eb3626
Frederik Creemers <frederikcreemers@gmail.com>
2018-11-21 17:19:57 +0100

    Refactor webapp handler that serves the frontend, and send accept header to dev server

--- internal/web/app.go
+++ internal/web/app.go
@@ -6,7 +6,6 @@ import (
 	"github.com/gobuffalo/packr"
 	"github.com/kataras/iris"
 	"github.com/kataras/iris/context"
-	"github.com/kataras/iris/core/router"
 	"github.com/marvin-automator/marvin/internal/auth"
 	"github.com/marvin-automator/marvin/internal/config"
 	"github.com/marvin-automator/marvin/internal/graphql"
@@ -33,86 +32,97 @@ func RunApp() error {
 	}
 
 	app.PartyFunc("/auth", auth.AuthHandlers)
-	app.PartyFunc("/", func(p router.Party) {
-		p.Use(auth.RequireLogin...)
 
-		p.Any("/graphql", func(ctx context.Context) {
-			h, err := graphql.GetHandler()
+	app.Any("/graphql", append(auth.RequireLogin, func(ctx context.Context) {
+		h, err := graphql.GetHandler()
+		if err != nil {
+			ctx.Writef("There was a problem setting up the GraphQL server %v", err)
+			return
+		}
+
+		h.ServeHTTP(ctx.ResponseWriter(), ctx.Request())
+	})...)
+
+	var frontendHandler context.Handler
+	// In development mode; just pass requests through to the React dev server.
+	if config.DevMode {
+		fmt.Println("Serving static assets in development mode.")
+		cmd := exec.Command("yarn", "start")
+		cmd.Dir = "./frontend"
+		err := cmd.Start()
+		frontendHandler = func(ctx context.Context) {
 			if err != nil {
-				ctx.Writef("There was a problem setting up the GraphQL server %v", err)
+				ctx.Writef("Error starting dev server %v.", err)
 				return
 			}
 
-			h.ServeHTTP(ctx.ResponseWriter(), ctx.Request())
-		})
+			url := "http://localhost:3000/" + ctx.Params().GetString("p")
+			fmt.Printf("Loading from %v\n", url)
+			req, err := http.NewRequest("GET", url, nil)
+			if err != nil {
+				ctx.Writef("Error constructing request to url %v", url)
+			}
 
-		// In development mode; just pass requests through to the React dev server.
-		if config.DevMode {
-			fmt.Println("Serving static assets in development mode.")
-			cmd := exec.Command("yarn", "start")
-			cmd.Dir = "./frontend"
-			err := cmd.Start()
-			p.HandleMany(iris.MethodGet, "/ /{path:path}", func(ctx context.Context) {
-				if err != nil {
-					ctx.Writef("Error starting dev server %v.", err)
-					return
-				}
+			accept := ctx.GetHeader("Accept")
+			fmt.Println("Accept:", accept)
+			req.Header.Add("Accept", accept)
 
-				url := "http://localhost:3000/" + ctx.Params().GetString("path")
-				fmt.Printf("Loading from %v\n", url)
-				resp, err := http.Get(url)
-				if err != nil {
-					ctx.Writef("Error Getting page: %v.", err)
-					return
-				}
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				ctx.Writef("Error Getting page: %v.", err)
+				return
+			}
 
-				ctx.StatusCode(resp.StatusCode)
-				for h, v := range resp.Header {
-					ctx.Header(h, strings.Join(v, ","))
-				}
+			ctx.StatusCode(resp.StatusCode)
+			for h, v := range resp.Header {
+				ctx.Header(h, strings.Join(v, ","))
+			}
 
-				body, err := ioutil.ReadAll(resp.Body)
-				if err != nil {
-					ctx.Writef("Error reading body %v", err)
-				}
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				ctx.Writef("Error reading body %v", err)
+			}
 
-				ctx.Write(body)
-			})
-		} else {
-			// Otherwise, we use packr to bundle in the built frontend
-			fmt.Println("Serving static assets in production mode.")
-			box := packr.NewBox("../../frontend/build")
-
-			p.Get("/{path:path}", func(ctx context.Context) {
-				f, err := box.Open(ctx.Params().GetString("path"))
-				if err == os.ErrNotExist {
-					f, err = box.Open("index.html")
-				}
-
-				if err != nil {
-					ctx.StatusCode(500)
-					ctx.Writef("Error getting file: %v", err)
-					return
-				}
-
-				info, err := f.Stat()
-
-				if err != nil {
-					ctx.StatusCode(500)
-					ctx.Writef("Error getting file: %v", err)
-					return
-				}
-
-				ftype := mime.TypeByExtension(filepath.Ext(info.Name()))
-				ctx.Header("Content-Type", ftype)
-				ctx.Header("Content-Length", strconv.Itoa(int(info.Size())))
-
-				body := make([]byte, info.Size())
-				f.Read(body)
-				ctx.Write(body)
-			})
+			ctx.Write(body)
 		}
-	})
+	} else {
+		// Otherwise, we use packr to bundle in the built frontend
+		fmt.Println("Serving static assets in production mode.")
+		box := packr.NewBox("../../frontend/build")
+
+		frontendHandler = func(ctx context.Context) {
+			f, err := box.Open(ctx.Params().GetString("path"))
+			if err == os.ErrNotExist {
+				f, err = box.Open("index.html")
+			}
+
+			if err != nil {
+				ctx.StatusCode(500)
+				ctx.Writef("Error getting file: %v", err)
+				return
+			}
+
+			info, err := f.Stat()
+
+			if err != nil {
+				ctx.StatusCode(500)
+				ctx.Writef("Error getting file: %v", err)
+				return
+			}
+
+			ftype := mime.TypeByExtension(filepath.Ext(info.Name()))
+			ctx.Header("Content-Type", ftype)
+			ctx.Header("Content-Length", strconv.Itoa(int(info.Size())))
+
+			body := make([]byte, info.Size())
+			f.Read(body)
+			ctx.Write(body)
+		}
+	}
+
+	frontendHandlers := append(auth.RequireLogin, frontendHandler)
+	app.Get("/", frontendHandlers...)
+	app.Get("{p:path}", frontendHandlers...)
 
 	return app.Run(iris.Addr(config.ServerHost))
 }
