513e9100d61407544a63ca2904783e1894323038
Frederik Creemers <frederikcreemers@gmail.com>
2018-11-22 13:53:44 +0100

    Create links in menu

--- frontend/src/layout/Menu.js
+++ frontend/src/layout/Menu.js
@@ -1,29 +1,30 @@
 import React from 'react'
 import {Icon, Menu, Segment, Sidebar } from 'semantic-ui-react'
+import {Link} from "@reach/router";
 
 const MarvinMenu = () => {
     return <Sidebar as={Menu} animation='overlay' icon='labeled' inverted vertical visible width='thin'>
-        <Menu.Item as='a'>
+        <Menu.Item as={Link} to="/">
             <Icon name='home' />
             Home
         </Menu.Item>
-        <Menu.Item as='a'>
+        <Menu.Item as={Link} to="/chores">
             <Icon name='tasks' />
             Chores
         </Menu.Item>
-        <Menu.Item as='a'>
+        <Menu.Item as={Link} to="/templates">
             <Icon name='file code outline' />
             Chore Templates
         </Menu.Item>
-        <Menu.Item as='a'>
+        <Menu.Item as={Link} to="/logs">
             <Icon name='list' />
             Logs
         </Menu.Item>
-        <Menu.Item as='a'>
+        <Menu.Item as={Link} to="/settings">
             <Icon name='cog' />
             Settings
         </Menu.Item>
-        <Menu.Item as='a'>
+        <Menu.Item as="a" href="/auth/logout" position="bottom">
             <Icon name='sign out' />
             Log out
         </Menu.Item>
