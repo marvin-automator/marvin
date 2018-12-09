cb10c4894768149ceb966e5ecb126a1028d5a817
Frederik Creemers <frederikcreemers@gmail.com>
2018-12-10 00:22:51 +0100

    Call the createChore mutation from the CreateChore component.

--- frontend/src/pages/CreateChore.js
+++ frontend/src/pages/CreateChore.js
@@ -1,8 +1,9 @@
 import React from "react";
-
+import withState from "recompose/withState"
 import { Query, Mutation } from "react-apollo";
+import { navigate } from "@reach/router"
 
-import {Form, Input, Divider, Button} from "semantic-ui-react";
+import {Form, Input, Button} from "semantic-ui-react";
 
 import {Message} from "semantic-ui-react";
 
@@ -11,32 +12,45 @@ import {CREATE_CHORE} from "../chores/queries";
 
 import ChoreTemplateEditor from "../choreTemplates/ChoreTemplateEditor";
 
-const CreateChore = ({templateId}) => {
+const enhancer = withState("tempChore", "setTempChore", {name: "",});
+const CreateChore = enhancer(({templateId, tempChore, setTempChore}) => {
     return <Query query={GET_TEMPLATE} variables={{id: templateId}}>
         {({loading, error, data}) => {
             if(loading) return <p>Loading...</p>;
             if(error) return <p>There was a problem: {error}</p>;
 
             data = data.ChoreTemplateById;
-            console.log(data);
 
-            return <React.Fragment>
-                <h1>Chore from: {data.name}</h1>
-                <Form>
-                    <Form.Field id="chore-name" name="name" label="Chore Name" control={Input} />
-                    <h2>Template Parameters</h2>
-                    {data.templateSettings.inputs.map((input) => {
-                        return <React.Fragment>
-                            <Form.Field id={input.name} name={input.name} label={input.name} control={Input} />
-                            {input.description ? <span>{input.description}</span> : null}
-                        </React.Fragment>
-                    })}
-                    <p><Button positive icon="add" type="submit" content="Create" /></p>
-                </Form>
-            </React.Fragment>
+            const handleChange = (e, {name, value}) => setTempChore({...tempChore, [name]: value});
+            let inputValues = Object.keys(tempChore).filter((k) => k.startsWith("input-")).map((key) => {
+                return {
+                    name: key.split("-")[1],
+                    value: tempChore[key]
+                }
+            });
+            return <Mutation mutation={CREATE_CHORE} variables={{name: tempChore.name, inputs: inputValues, templateId: templateId}}
+                             onCompleted={({createChore:{id}}) => navigate(`/chores/${id }`)}>
+                {(createChore, {error}) => {
+                    return <React.Fragment>
+                        <h1>Chore from: {data.name}</h1>
+                        <Form onSubmit={createChore}>
+                            <Form.Field id="chore-name" name="name" label="Chore Name" control={Input} onChange={handleChange} />
+                            <h2>Template Parameters</h2>
+                            {data.templateSettings.inputs.map((input) => {
+                                return <React.Fragment key={input.name}>
+                                    <Form.Field id={input.name} name={`input-${input.name}`} label={input.name} control={Input}
+                                                onChange={handleChange} />
+                                    {input.description ? <p>{input.description}</p> : null}
+                                </React.Fragment>
+                            })}
+                            <p><Button positive icon="add" type="submit" content="Create" /></p>
+                        </Form>
+                    </React.Fragment>
+                }}
+            </Mutation>
         }}
     </Query>
-};
+});
 
 const updateCache = (cache, { data: { updateChoreTemplate } }) => {
 
