e93dfbba4966ca473009ee512550b12da91e09f1
Frederik Creemers <frederikcreemers@gmail.com>
2019-01-03 21:00:02 +0100

    Allow storing oauth scopes on an account

--- internal/oauth2/accounts.go
+++ internal/oauth2/accounts.go
@@ -6,7 +6,8 @@ type AccountGetter func(t *oauth2.Token) (Account, error)
 
 type Account struct {
 	Id       string        `json:"id"`
-	Token    *oauth2.Token `json:"token"`
+	Token    *oauth2.Token `json:"-"`
+	Scopes   []string      `json:"scopes"`
 	Name     string        `json:"name"`
 	ImageURL string        `json:"image_url"`
 }
